a22388e52ac077beaedde65674135965f164bcc2
fakh <fakhridin008@gmail.com>
2025-11-22 17:47:20 +0300

    driver_list dob

--- drivers/views.py
+++ drivers/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from .models import *
 
 
 def driver_list(request):
     drivers = Drivers.objects.all()
-    return render(request, 'drivers/driver_list.html', {'drivers': drivers})
+    return render(request, 'drivers/drivers_list.html', {'drivers': drivers})
+
+def driver_detail(request, driver_id):
+    driver = Drivers.objects.get(id=driver_id)
+    if not driver:
+        return HttpResponse('Driver not found')
+    return render(request, 'drivers/driver_detail.html', {'driver': driver})
