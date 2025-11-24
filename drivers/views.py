9f855f6469de6e93693ec4022d3576199c7749ad
fakh <fakhridin008@gmail.com>
2025-11-24 14:59:25 +0300

    Drivers koren

--- drivers/views.py
+++ drivers/views.py
@@ -12,3 +12,22 @@ def driver_detail(request, driver_id):
     if not driver:
         return HttpResponse('Driver not found')
     return render(request, 'drivers/driver_detail.html', {'driver': driver})
+
+def driver_update(request, driver_id):
+    driver = Drivers.objects.filter(id=driver_id).first()
+    if not driver:
+        return HttpResponse('Driver not found')
+    if request.method == 'GET':
+        return render(request, 'drivers/driver_update.html', {'driver': driver})
+    elif request.method == 'POST':
+        driver.name = request.POST.get('name')
+        driver.phone_number = request.POST.get('phone_number')
+        driver.car_model = request.POST.get('car_model')
+        driver.car_number = request.POST.get('car_number')
+        driver.rating = request.POST.get('rating')
+        driver.status = request.POST.get('status') == 'on'
+        driver.save()
+        return redirect('driver_detail', driver_id=driver.id) 
+    
+
+    
