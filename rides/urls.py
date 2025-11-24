ca6f0e3f66f8a92f9c2a79f45f6599bb3a8b047f
Binyomin <binyomin927@gmail.com>
2025-11-24 17:05:34 +0500

    add static

--- rides/urls.py
+++ rides/urls.py
@@ -1,10 +1,4 @@
 from django.urls import path
-<<<<<<< HEAD
-
-
-urlpatterns = [
-    
-=======
 from .views import *
 
 urlpatterns = [
@@ -13,5 +7,4 @@ urlpatterns = [
     path("<int:ride_id>/", ride_detail, name="ride_detail"),
     path("<int:ride_id>/update/", ride_update, name="ride_update"),
     path("<int:ride_id>/delete/", ride_delete, name="ride_delete"),
->>>>>>> e05827da18bb32d36262b0d5d332ba8484db5ad4
 ]
