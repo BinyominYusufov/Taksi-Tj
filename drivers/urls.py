62cae653f946d01222c02aee2e585bc612554118
fakh <fakhridin008@gmail.com>
2025-11-24 15:14:12 +0300

    Create

--- drivers/urls.py
+++ drivers/urls.py
@@ -4,6 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('', driver_list, name='driver_list'),
+    path('create/', driver_create, name='driver_create'),
     path('<int:driver_id>/', driver_detail, name='driver_detail'),
     path('<int:driver_id>/update/', driver_update, name='driver_update'),
 ]
